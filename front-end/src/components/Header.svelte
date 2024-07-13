a1e6ccaf8fd4b7650ccc821eac35a362195f5b78
mb <mb@example.com>
2024-07-13 22:42:32 +0200

    set up svelte

---
+++ front-end/src/components/Header.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let active;
+</script>
+<nav>
+    <ul>
+        <li><a class:active={active === "/"} href="/">Home</a></li>
+    </ul>
+</nav>
+
+<style>
+    ul {
+        align-content: end;
+        list-style-type: none;
+        margin-block-start: 0px;
+        margin-block-end: 0px;
+        padding-inline-start: 0px;
+        background: #009878;
+
+    }
+    li a{
+        text-decoration: none;
+        color: white;
+        padding: 1em;
+        float: left;
+    }
+    li a:hover{
+        background: #53d0b4;
+    }
+
+    a.active {
+        font-weight: bold;
+
+    }
+</style>
